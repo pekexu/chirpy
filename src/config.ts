af9c421bd642d22e37397e6c83a3a66ea1625fab
pekexu <pekka.tuomi@gmail.com>
2025-10-23 18:35:04 +0000

    configuring drizzle orm

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
+
 type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
 };
 
+process.loadEnvFile();
+
+function envOrThrow(key: string){
+    const value = process.env[key];
+    if (!value){
+        throw new Error(`Missing key: ${key}`);
+    }
+     return value;
+}
 export const config: APIConfig = {
-    fileserverHits: 0
+    fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
+
