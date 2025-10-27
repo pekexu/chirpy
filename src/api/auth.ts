3286830f282b217b4a0932174b8ad56dca28b690
pekexu <pekka.tuomi@gmail.com>
2025-10-27 11:36:01 +0000

    authentication

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,13 @@
 import * as argon2 from "argon2";
+import * as jwt from "jsonwebtoken";
 import { BadRequestError, respondWithJSON, UnauthorizedError } from "./errorhandler.js";
 import { Request, Response } from "express";
 import { db } from "../db/index.js";
 import { UserResponse, users } from "../db/schema.js";
 import { eq } from "drizzle-orm";
+
+
+const TOKEN_ISSUER = "chirpy";
 
 export async function hashPassword(password: string): Promise<string>{
     try {     
@@ -14,8 +18,40 @@ export async function hashPassword(password: string): Promise<string>{
     }
 }
 
-export async function checkPasswordHash(password: string, hash: string): Promise<boolean>{
+export function validateJWT(tokenString: string, secret: string): string {
+  let decoded: payload;
 
+  try {
+    decoded = jwt.verify(tokenString, secret) as jwt.JwtPayload;
+  } catch  (e) {
+    throw new UnauthorizedError("Invalid token");
+  }
+  if (decoded.iss !== TOKEN_ISSUER) {
+    throw new UnauthorizedError("Invalid issuer");
+  }
+  if (!decoded.sub) {
+    throw new UnauthorizedError("No user ID in token");
+  }
+  return decoded.sub;
+}
+
+
+export type payload = Pick<jwt.JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string{
+    const load:payload = {
+        iss: TOKEN_ISSUER,
+        sub: userID,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000)+expiresIn,
+    }
+    const signIn = jwt.sign(load, secret);
+    return signIn;
+}
+
+
+export async function checkPasswordHash(password: string, hash: string): Promise<boolean>{
     try {
         if(await argon2.verify(hash,password)){
             return true;
@@ -50,3 +86,4 @@ export async function handlerUserLogin(req: Request, res: Response){
   }
   respondWithJSON(res, 200, userRes);
 }
+
